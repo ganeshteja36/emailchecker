7658cefb12d4c8da87609d4160d3a9c5ef8cac92
ganeshteja36 <57834999+ganeshteja36@users.noreply.github.com>
2019-11-16 19:13:18 +0530

    Create emailchecker.py

---
+++ emailchecker.py
@@ -0,0 +1,9 @@
+import re
+#simple regex expression for email checking  
+pattern =r"([\w\.-]+)@([\w\.-]+)([\w\.-]+)"
+email=input("enter your email:")
+if re.search(pattern,email):
+    print(email)
+else:
+    print('yor email is incorrect')
+
